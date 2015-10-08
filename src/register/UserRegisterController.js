eeed5d54a1605f6e9d6e06288eeced88ed996f1a
RDuX <ruben.dens@gmail.com>
2015-10-08 15:47:01 +0200

    registration functionality

--- src/register/UserRegisterController.js
+++ src/register/UserRegisterController.js
@@ -3,24 +3,47 @@
 /**
  * @ngInject
  */
-var UserRegisterController = function ($scope, $controller, $routeParams, User) {
+var UserRegisterController = function ($scope, $http, $location, $routeParams, npolarApiConfig, NpolarApiMessage, User) {
 
-  // Extend -> NpolarEditController
-  $controller('NpolarEditController', { $scope: $scope });
-  
-  if ($scope.security.isAuthorized('read', User.path)) {
+  const message = NpolarApiMessage;
+  const registrationUri = "https://"+npolarApiConfig.base.split("//")[1]+"/user/register";
+  const confirmationUri = "https://"+npolarApiConfig.base.split("//")[1]+"/user/confirm";
 
-    // Person -> npolarApiResource -> ngResource
-    $scope.resource = User;
-  
-    // Formula ($scope.formula is set by parent)
-    $scope.formula.schema = '//api.npolar.no/schema/user-1';
-    $scope.formula.form = 'edit/formula.json';
-    
-    // edit (or new) action
-    $scope.edit();
-  }
+  $scope.resource = User;
+  $scope.user = {link: `https://${window.location.host}/user/confirm`};
 
+  $scope.compare = function(user) {
+    if (user.password2 !== undefined) {
+      if (user.password !== user.password2) {
+        $scope.pwdStyle={color: 'rgb(215,10,83)', 'border-color': 'rgb(215,10,83)'};
+      } else {
+        $scope.pwdStyle={color: 'rgb(34,139,34)', 'border-color': 'rgb(34,139,34)'};
+      }
+
+      if (user.password2 === "") {
+        $scope.pwdStyle={};
+      }
+    }
+  };
+
+  $scope.register = function(user) {
+    if (user.password !== user.password2) {
+      message.emit("npolar-api-error", "Password mismatch. make sure the passwords are written the same");
+    } else {
+      $scope.user = user;
+      var req = { method: "POST", url: registrationUri, data: $scope.user };
+      $http(req).success(reponse => {$location.path('.');}).error(
+        response => { message.emit("npolar-api-error", "Registration failed"); }
+      );
+    }
+  };
+
+  $scope.confirm = function(id) {
+      var req = { method: "GET", url: confirmationUri+$routeParams.id };
+      $http(req).success(response => { $location.path('.');}).error(
+        response => { message.emit("npolar-api-error", "Confirmation failed"); }
+      );
+  };
 };
 
 module.exports = UserRegisterController;
