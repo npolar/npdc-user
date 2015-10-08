f06e15a828d1d7d7ce1e370447f5e0fd547b8040
RDuX <ruben.dens@gmail.com>
2015-10-08 15:47:44 +0200

    added confirmation route

--- src/router.js
+++ src/router.js
@@ -10,6 +10,9 @@ var router = function ($routeProvider, $locationProvider) {
   $routeProvider.when('/register', {
     template: require('./register/register-user.html'),
     controller: 'UserRegisterController'
+  }).when('/confirm/:id', {
+    template: require('./register/confirm.html'),
+    controller: 'UserRegisterController'
   }).when('/:id', {
     template: require('./show/show-user.html'),
     controller: 'UserShowController'
