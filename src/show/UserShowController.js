1b9fba5371737a20f676d955975884db404f8716
RDuX <ruben.dens@gmail.com>
2015-11-09 10:53:48 +0100

    layout tweaks

--- src/show/UserShowController.js
+++ src/show/UserShowController.js
@@ -2,11 +2,11 @@
 /**
  * @ngInject
  */
-var PersonShowController = function ($scope, $controller, $routeParams, Person, User) {
-  
+var PersonShowController = function ($scope, $controller, $routeParams, Person, User, npdcAppConfig) {
+
   $controller('NpolarBaseController', {$scope: $scope});
   $scope.resource = User;
-  
+
   $scope.personHref = function(uri) {
     if (/api\.npolar\.no/.test(uri)) {
       let id = uri.split('/').slice(-1)[0];
@@ -14,16 +14,16 @@ var PersonShowController = function ($scope, $controller, $routeParams, Person, 
     } else {
       return '';
     }
-    
+
   };
-  
+
   let id = $routeParams;
-  
+
   let show = function(resource, id) {
-  
+
     let invariants = { fields: "_id,name,systems,groups,active" };
     let query = Object.assign(id, invariants);
-    
+
     return resource.fetch(query, function(user) {
       if (!user.name) {
         user.name = `${user.first_name} ${user.last_name}`;
@@ -33,17 +33,19 @@ var PersonShowController = function ($scope, $controller, $routeParams, Person, 
       $scope.document = user;
     });
   };
-  
-  
+
+
   if ($scope.security.isAuthorized('read', User.path)) {
-    show(User, id);
+    show(User, id).$promise.then(data => {
+      npdcAppConfig.cardTitle = data.name;
+    });
   } else {
-    
+
     // Not authorized, switch to the Person API until https://github.com/npolar/api.npolar.no/issues/63
     id = { id: $routeParams.id.split('@')[0] } ;
     show(Person, id);
   }
-  
+
 };
 
 module.exports = PersonShowController;
