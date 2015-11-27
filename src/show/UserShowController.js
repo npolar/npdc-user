d4df03dd8ad7a0aa1323e1e4a4ea626d7e4b2ac2
Anders Bälter <anders.balter@npolar.no>
2015-11-27 15:19:08 +0100

    Fix broken code

--- src/show/UserShowController.js
+++ src/show/UserShowController.js
@@ -21,16 +21,25 @@ var PersonShowController = function ($scope, $controller, $routeParams, Person, 
 
   let show = function(resource, id) {
 
-    let invariants = { fields: "_id,name,systems,groups,active" };
+    let invariants = { fields: "_id,name,systems,groups,active,created,updated,uri" };
     let query = Object.assign(id, invariants);
 
     return resource.fetch(query, function(user) {
       if (!user.name) {
-        user.name = `${user.first_name} ${user.last_name}`;
-        user.active = user.currently_employed;
-        user = { name: user.name, active: user.active };
+        user.name = user.first_name ? `${user.first_name} ${user.last_name}`: user._id;
+        user.active = user.active || user.currently_employed;
       }
       $scope.document = user;
+      $scope._error = false;
+    }, function(errorData) {
+      if (errorData.status === 404) {
+        $scope._error = "Couldn't find document \"" + $routeParams.id + "\"";
+      } else {
+        $scope._error = "Couldn't load document :(";
+        if (errorData.statusText && errorData.statusText.length > 0) {
+          $scope._error += ". Status: " + errorData.status + ", Message: " + errorData.statusText;
+        }
+      }
     });
   };
 
